e48dced20569176ac38e89b96ed65156d0db7f9a
Arpit Dixit <arpitd@dewsolutions.in>
2021-06-23 19:31:04 +0530

    Initial Commit

---
+++ SwiftUI-Podcast_Player/SwiftUI_Podcast_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Podcast_PlayerApp.swift
+//  SwiftUI-Podcast_Player
+//
+//  Created by Arpit Dixit on 23/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Podcast_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
